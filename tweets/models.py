bfdbd12aba5003eee48dcad3099d45af261c6b00
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2022-11-02 18:48:01 +0100

    Tweet Model Fix

--- tweets/models.py
+++ tweets/models.py
@@ -6,7 +6,7 @@ class Tweet(models.Model):
     creation_datetime = models.DateTimeField(auto_now_add=True)
     body = models.CharField(max_length=255)
     parent_tweet = models.ForeignKey('Tweet', on_delete=models.SET_NULL, default=None, blank=True, null=True)
-    likes = models.ManyToManyField('users.User', related_name='likes')
+    likes = models.ManyToManyField('users.User', related_name='likes', blank=True)
 
     def __str__(self):
         return self.body[:20] if len(self.body) > 20 else self.body
