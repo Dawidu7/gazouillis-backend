f8d98c141072973d55e56ed466ebf4625c4607bb
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2022-11-10 15:08:30 +0100

    Cors Methods

--- gazouillis/settings.py
+++ gazouillis/settings.py
@@ -150,5 +150,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
 
+CORS_ALLOW_METHODS = ['GET', 'POST', 'DELETE', 'PUT']
+
 
 CSRF_TRUSTED_ORIGINS = ['https://gazouillis-b.up.railway.app']
