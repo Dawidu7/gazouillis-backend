33b390a06fcf0fc913100459010dbde9005bb200
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2022-11-10 15:02:48 +0100

    Changed Cors Allowed Origins

--- gazouillis/settings.py
+++ gazouillis/settings.py
@@ -146,7 +146,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 
 CSRF_TRUSTED_ORIGINS = ['https://gazouillis-b.up.railway.app']
